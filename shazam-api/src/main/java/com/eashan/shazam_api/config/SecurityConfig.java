cf692dc46da4e2a6c48d2455da55505c84df05f1
abit987 <kurichheashan@gmail.com>
2025-07-26 17:49:11 +0530

    Configure CORS for Netlify frontend URL

--- shazam-api/src/main/java/com/eashan/shazam_api/config/SecurityConfig.java
+++ shazam-api/src/main/java/com/eashan/shazam_api/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowCredentials(true);
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173","https://veritasvi.netlify.app"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setExposedHeaders(List.of("Authorization"));
